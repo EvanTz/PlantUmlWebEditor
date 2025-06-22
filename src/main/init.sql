abbbfc7475262ec35551bd8d90bd43f106933128
EvanTz <EvanTz@users.noreply.github.com>
2025-06-22 18:59:06 +0300

    code

--- src/main/init.sql
+++ src/main/init.sql
@@ -6,36 +6,36 @@ CREATE DATABASE plantuml;
 
 -- Create tables
 CREATE TABLE users (
-                       id BIGSERIAL PRIMARY KEY,
-                       username VARCHAR(20) UNIQUE NOT NULL,
-                       email VARCHAR(50) UNIQUE NOT NULL,
-                       password VARCHAR(120) NOT NULL,
-                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                       updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+id BIGSERIAL PRIMARY KEY,
+username VARCHAR(20) UNIQUE NOT NULL,
+email VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(120) NOT NULL,
+created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE roles (
-                       id BIGSERIAL PRIMARY KEY,
-                       name VARCHAR(20) NOT NULL
+id BIGSERIAL PRIMARY KEY,
+name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE user_roles (
-                            user_id BIGINT NOT NULL,
-                            role_id BIGINT NOT NULL,
-                            PRIMARY KEY (user_id, role_id),
-                            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-                            FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+user_id BIGINT NOT NULL,
+role_id BIGINT NOT NULL,
+PRIMARY KEY (user_id, role_id),
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
 
 CREATE TABLE projects (
-                          id BIGSERIAL PRIMARY KEY,
-                          name VARCHAR(100) NOT NULL,
-                          description VARCHAR(500),
-                          content TEXT,
-                          user_id BIGINT NOT NULL,
-                          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+id BIGSERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+description VARCHAR(500),
+content TEXT,
+user_id BIGINT NOT NULL,
+created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 
